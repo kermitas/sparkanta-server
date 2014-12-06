2651a6ddabeb11563e16f4202d2a2e09ae3106f8
Arthur Stanek <artur@biodiscovery.com>
2014-12-06 01:13:02 +0100

    OutgoingMessageListener was rewritten to be FSM.

--- sparkanta-tcp-rest-gateway/src/main/scala/as/sparkanta/ama/actor/tcp/message/OutgoingMessageListener.scala
+++ sparkanta-tcp-rest-gateway/src/main/scala/as/sparkanta/ama/actor/tcp/message/OutgoingMessageListener.scala
@@ -1,11 +1,29 @@
 package as.sparkanta.ama.actor.tcp.message
 
-import akka.actor.{ ActorRef, Actor, ActorLogging, OneForOneStrategy, SupervisorStrategy }
+import akka.actor.{ ActorRef, FSM, OneForOneStrategy, SupervisorStrategy }
+import akka.io.Tcp
 import as.akka.broadcaster.Broadcaster
 import as.sparkanta.ama.config.AmaConfig
 import java.net.InetSocketAddress
 import as.sparkanta.internal.message.MessageToDevice
 import as.sparkanta.device.message.{ MessageToDevice => MessageToDeviceMarker }
+import scala.collection.mutable.ListBuffer
+import java.io.{ DataOutputStream, ByteArrayOutputStream }
+import akka.util.{ FSMSuccessOrStop, ByteString }
+
+object OutgoingMessageListener {
+  sealed trait State extends Serializable
+  case object WaitingForSomethingToSend extends State
+  case object WaitingForAck extends State
+
+  sealed trait StateData extends Serializable
+  case object WaitingForSomethingToSendStateData extends StateData
+  case object WaitingForAckStateData extends StateData
+
+  sealed trait Message extends Serializable
+  sealed trait InternalMessage extends Message
+  object Ack extends InternalMessage with Tcp.Event
+}
 
 class OutgoingMessageListener(
   amaConfig:     AmaConfig,
@@ -13,31 +31,99 @@ class OutgoingMessageListener(
   localAddress:  InetSocketAddress,
   tcpActor:      ActorRef,
   runtimeId:     Long
-) extends Actor with ActorLogging {
+) extends FSM[OutgoingMessageListener.State, OutgoingMessageListener.StateData] with FSMSuccessOrStop[OutgoingMessageListener.State, OutgoingMessageListener.StateData] {
+
+  import OutgoingMessageListener._
+
+  protected val outgoingBuffer = new ListBuffer[MessageToDeviceMarker]
 
   override val supervisorStrategy = OneForOneStrategy() {
     case t => {
-      log.warning(s"Terminating because once of child actors failed ($t).")
-      context.stop(self) // TODO: stop FSM actor
+      stop(FSM.Failure(new Exception("Terminating because once of child actors failed.", t)))
       SupervisorStrategy.Escalate
     }
   }
 
-  override def receive = {
-    case messageToDevice: MessageToDevice => sendMessageToDevice(messageToDevice.messageToDevice)
-    case message                          => log.warning(s"Unhandled $message send by ${sender()}")
+  startWith(WaitingForSomethingToSend, WaitingForSomethingToSendStateData)
+
+  when(WaitingForSomethingToSend) {
+    case Event(messageToDevice: MessageToDevice, WaitingForSomethingToSendStateData) => successOrStopWithFailure { sendRequestWhileNothingToDo(messageToDevice.messageToDevice) }
   }
+
+  when(WaitingForAck) {
+    case Event(Ack, WaitingForAckStateData)                              => successOrStopWithFailure { ackReceived }
+    case Event(messageToDevice: MessageToDevice, WaitingForAckStateData) => successOrStopWithFailure { sendRequestWhileWaitingForAck(messageToDevice.messageToDevice) }
+  }
+
+  onTransition {
+    case fromState -> toState => log.info(s"State change from $fromState to $toState")
+  }
+
+  whenUnhandled {
+    case Event(unknownMessage, stateData) => {
+      log.warning(s"Received unknown message '$unknownMessage' in state $stateName (state data $stateData)")
+      stay using stateData
+    }
+  }
+
+  onTermination {
+    case StopEvent(reason, currentState, stateData) => terminate(reason, currentState, stateData)
+  }
+
+  initialize
 
   override def preStart(): Unit = {
     // notifying broadcaster to register us with given classifier
     amaConfig.broadcaster ! new Broadcaster.Register(self, new OutgoingMessageListenerClassifier(runtimeId))
   }
 
-  protected def sendMessageToDevice(messageToDevice: MessageToDeviceMarker): Unit = {
-    // TODO: (change this actor into FSM)
-    // TODO: serialize to byte protocol
-    // TODO: add to outgoing buffer
-    // TODO: if was in stage of waiting for ack then nothing; if was in stage "waiting for something to send" then send this into wire and change state to wait for ack
-    // TODO: if was in waiting for ack and ack comes then check if there is something to send, if yes then send and stay in state, if not then back to waiting for something to send
+  protected def sendRequestWhileNothingToDo(messageToDevice: MessageToDeviceMarker) = {
+    serializeAndSendToWire(messageToDevice)
+    goto(WaitingForAck) using WaitingForAckStateData
+  }
+
+  protected def sendRequestWhileWaitingForAck(messageToDevice: MessageToDeviceMarker) = {
+    outgoingBuffer += messageToDevice
+    stay using WaitingForAckStateData
+  }
+
+  protected def serializeAndSendToWire(messageToDevice: MessageToDeviceMarker): Unit = {
+    val headerWithMessageToDeviceAsByteArray = addMessageHeader(serialize(messageToDevice))
+    tcpActor ! new Tcp.Write(ByteString(headerWithMessageToDeviceAsByteArray), Ack)
+  }
+
+  protected def ackReceived = if (outgoingBuffer.isEmpty) {
+    goto(WaitingForSomethingToSend) using WaitingForSomethingToSendStateData
+  } else {
+    val messageToDevice = outgoingBuffer.head
+    outgoingBuffer -= messageToDevice
+    serializeAndSendToWire(messageToDevice)
+    stay using WaitingForAckStateData
+  }
+
+  protected def serialize(messageToDevice: MessageToDeviceMarker): Array[Byte] = ??? // TODO !!
+
+  protected def addMessageHeader(messageToDeviceAsByteArray: Array[Byte]): Array[Byte] = {
+    val baos = new ByteArrayOutputStream
+    val dos = new DataOutputStream(baos)
+    dos.writeShort(messageToDeviceAsByteArray.length)
+    dos.write(messageToDeviceAsByteArray)
+    dos.flush
+    baos.toByteArray
+  }
+
+  protected def terminate(reason: FSM.Reason, currentState: OutgoingMessageListener.State, stateData: OutgoingMessageListener.StateData) = reason match {
+
+    case FSM.Normal => {
+      log.debug(s"Stopping (normal), state $currentState, data $stateData, runtimeId $runtimeId.")
+    }
+
+    case FSM.Shutdown => {
+      log.debug(s"Stopping (shutdown), state $currentState, data $stateData, runtimeId $runtimeId.")
+    }
+
+    case FSM.Failure(cause) => {
+      log.warning(s"Stopping (failure, cause $cause), state $currentState, data $stateData, runtimeId $runtimeId.")
+    }
   }
 }
