d07f7eed275f41952824bec1034915ade51bee6b
Arthur Stanek <artur@biodiscovery.com>
2014-12-08 22:47:48 +0100

    Added sending of ping to test.

--- sparkanta-tcp-rest-gateway/src/test/scala/HelloCommandSenderTest.scala
+++ sparkanta-tcp-rest-gateway/src/test/scala/HelloCommandSenderTest.scala
@@ -2,7 +2,7 @@ import org.scalatest.{ FeatureSpec, Matchers }
 
 import java.net.Socket
 import scala.io.StdIn
-import as.sparkanta.device.message.{ Hello, Message65536LengthHeader }
+import as.sparkanta.device.message.{ Hello, Ping, Message65536LengthHeader }
 import as.sparkanta.device.message.serialize.Serializers
 
 class HelloCommandSenderTest extends FeatureSpec with Matchers {
@@ -11,21 +11,31 @@ class HelloCommandSenderTest extends FeatureSpec with Matchers {
 
     val identificationStringWithSoftwareVersion = {
       val identificationString = "SPARKANTA"
-      val softwareVersion: Byte = 12
+      val softwareVersion: Byte = 1
 
       identificationString.getBytes ++ Array[Byte](softwareVersion.toByte)
     }
 
+    val serializers = new Serializers
+    val messageLengthHeader = new Message65536LengthHeader
+
     val helloMessageAsByteArray = {
-      val hello = new Hello("Alice has a cat")
-      val helloAsByteArray = new Serializers().serialize(hello)
-      new Message65536LengthHeader().prepareMessageToGo(helloAsByteArray)
+      val message = new Hello("Alice has a cat")
+      val messageAsByteArray = serializers.serialize(message)
+      messageLengthHeader.prepareMessageToGo(messageAsByteArray)
+    }
+
+    val pingMessageAsBytes = {
+      val message = new Ping
+      val messageAsByteArray = serializers.serialize(message)
+      messageLengthHeader.prepareMessageToGo(messageAsByteArray)
     }
 
     val socket = new Socket("localhost", 8080)
 
     socket.getOutputStream.write(identificationStringWithSoftwareVersion)
     socket.getOutputStream.write(helloMessageAsByteArray)
+    socket.getOutputStream.write(pingMessageAsBytes)
     socket.getOutputStream.flush
 
     StdIn.readLine()
