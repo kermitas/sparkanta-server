171a3a41c5af1edde6f1766daff632d6f258cf89
Arthur Stanek <artur@biodiscovery.com>
2014-12-14 16:11:38 +0100

    Make MessageLengthHeader more universal as abstract class (not a trait).

--- sparkanta-tcp-rest-gateway/src/main/scala/as/sparkanta/device/message/length/Message65536LengthHeader.scala
+++ sparkanta-tcp-rest-gateway/src/main/scala/as/sparkanta/device/message/length/Message65536LengthHeader.scala
@@ -2,26 +2,16 @@ package as.sparkanta.device.message.length
 
 import java.io.{ ByteArrayOutputStream, ByteArrayInputStream, DataOutputStream, DataInputStream }
 
-class Message65536LengthHeader extends MessageLengthHeader {
+class Message65536LengthHeader extends MessageLengthHeader(2) {
 
-  lazy final val messageHeaderLength: Int = 2
+  protected override def safeReadMessageLength(messageAsByteArray: Array[Byte]): Int = new DataInputStream(new ByteArrayInputStream(messageAsByteArray)).readShort
 
-  lazy final val maxMessageLength: Long = Math.pow(256, messageHeaderLength).toLong
-
-  override def readMessageLength(messageAsByteArray: Array[Byte]): Int = if (messageAsByteArray.length >= messageHeaderLength) {
-    new DataInputStream(new ByteArrayInputStream(messageAsByteArray)).readShort
-  } else {
-    throw new IllegalArgumentException(s"Passed byte array length should be at least $messageHeaderLength bytes long (currently it is ${messageAsByteArray.length}).")
-  }
-
-  override def prepareMessageToGo(messageAsByteArray: Array[Byte]): Array[Byte] = if (messageAsByteArray.length <= maxMessageLength) {
+  protected override def safePrepareMessageToGo(messageAsByteArray: Array[Byte]): Array[Byte] = {
     val baos = new ByteArrayOutputStream
     val dos = new DataOutputStream(baos)
     dos.writeShort(messageAsByteArray.length)
     dos.write(messageAsByteArray)
     dos.flush
     baos.toByteArray
-  } else {
-    throw new IllegalArgumentException(s"Passed array length can be maximally $maxMessageLength bytes long (currently it is ${messageAsByteArray.length}).")
   }
 }
--- sparkanta-tcp-rest-gateway/src/main/scala/as/sparkanta/device/message/length/MessageLengthHeader.scala
+++ sparkanta-tcp-rest-gateway/src/main/scala/as/sparkanta/device/message/length/MessageLengthHeader.scala
@@ -1,13 +1,26 @@
 package as.sparkanta.device.message.length
 
-trait MessageLengthHeader {
+/**
+ * @param messageHeaderLength in bytes, for example 1 (that means messages of length from 0 to 256)
+ */
+abstract class MessageLengthHeader( final val messageHeaderLength: Int) {
 
-  def messageHeaderLength: Int
+  final val maxMessageLength: Int = Math.pow(256, messageHeaderLength).toInt
 
-  def maxMessageLength: Long
+  def readMessageLength(messageAsByteArray: Array[Byte]): Int = if (messageAsByteArray.length >= messageHeaderLength) {
+    safeReadMessageLength(messageAsByteArray)
+  } else {
+    throw new IllegalArgumentException(s"Passed byte array length should be at least $messageHeaderLength bytes long (currently it is ${messageAsByteArray.length}).")
+  }
 
-  def readMessageLength(messageAsByteArray: Array[Byte]): Int
+  protected def safeReadMessageLength(messageAsByteArray: Array[Byte]): Int
 
-  def prepareMessageToGo(messageAsByteArray: Array[Byte]): Array[Byte]
+  def prepareMessageToGo(messageAsByteArray: Array[Byte]): Array[Byte] = if (messageAsByteArray.length <= maxMessageLength) {
+    safePrepareMessageToGo(messageAsByteArray: Array[Byte])
+  } else {
+    throw new IllegalArgumentException(s"Passed array length can be maximally $maxMessageLength bytes long (currently it is ${messageAsByteArray.length}).")
+  }
+
+  protected def safePrepareMessageToGo(messageAsByteArray: Array[Byte]): Array[Byte]
 }
 
